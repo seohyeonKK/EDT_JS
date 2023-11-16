86bae9493f52c8ea69ce96855a82ac1b3431bc0e
seohyeonKK <sts05158@gmail.com>
2023-11-16 14:28:05 +0900

    webpack 플러그인 및 chunkhash 추가

--- RestaurantSimulation/webpack.config.js
+++ RestaurantSimulation/webpack.config.js
@@ -1,10 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	// 번들링 시작점
 	entry: "./src/main.js",
 	output: {
-		filename: "main.js",
+		filename: "main.[chunkhash].js",
 		path: path.resolve(__dirname, "dist"),
+		clean: true, // dist폴더 정리
 	},
+	plugins: [
+		// 동적인 번들 파일명을 위해 html 파일 자동 생성 플러그인
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.html",
+			chunks: ["main"],
+		}),
+	],
+	// 디버깅을 위해 번들 파일과 원본 소스 mapping
+	devtool: "source-map",
 };
