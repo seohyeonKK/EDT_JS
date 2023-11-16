e422cae618869e94eb0ca0f26560345df73efa3d
seohyeonKK <sts05158@gmail.com>
2023-11-16 09:52:03 +0900

    코드 포매팅

--- RestaurantSimulation/Chef.js
+++ RestaurantSimulation/Chef.js
@@ -1,41 +1,40 @@
-export class Chef{
-  status = '대기중';
+export class Chef {
+	status = "대기중";
 
-  constructor(name){
-    this.name = name;
-    this.list = this.addChefList();
-  }
+	constructor(name) {
+		this.name = name;
+		this.list = this.addChefList();
+	}
 
-  cooking(food){
-    return new Promise((resolve, reject) => {
-      this.status = '요리중';
-      this.updateChefList(food);
-      setTimeout(() => {
-        this.status = '대기중';
-        this.updateChefList(null);
-        resolve(food);
-      }, food.time);
-    });
-  }
+	cooking(food) {
+		return new Promise((resolve, reject) => {
+			this.status = "요리중";
+			this.updateChefList(food);
+			setTimeout(() => {
+				this.status = "대기중";
+				this.updateChefList(null);
+				resolve(food);
+			}, food.time);
+		});
+	}
 
-  addChefList(){
-    const newList = document.createElement("li");
+	addChefList() {
+		const newList = document.createElement("li");
 
-    const textNode = document.createTextNode(`${this.name} 대기중`);
-    newList.append(textNode);
-    
-    const myList = document.getElementById("cooking");
-    myList.appendChild(newList);
-    
-    return newList;
-  }
+		const textNode = document.createTextNode(`${this.name} 대기중`);
+		newList.append(textNode);
 
-  updateChefList(food){
-    if (this.status === '대기중'){
-      this.list.textContent = `${this.name} 대기중`
-    }
-    else{
-      this.list.textContent = `${this.name} 주문${food.no} ${food.name} 요리중`
-    }
-  }
+		const myList = document.getElementById("cooking");
+		myList.appendChild(newList);
+
+		return newList;
+	}
+
+	updateChefList(food) {
+		if (this.status === "대기중") {
+			this.list.textContent = `${this.name} 대기중`;
+		} else {
+			this.list.textContent = `${this.name} 주문${food.no} ${food.name} 요리중`;
+		}
+	}
 }
--- RestaurantSimulation/main.js
+++ RestaurantSimulation/main.js
@@ -11,47 +11,51 @@ let servers = [new Server(1000, "수지"), new Server(2000, "철수")];
 let orderList = [];
 let waitingList = [];
 
-
-document.getElementById("soondae-soup").addEventListener("click", () => getOrder(new Food(soondae)));
-document.getElementById("hangover-soup").addEventListener("click", () => getOrder(new Food(hangover)));
+document
+	.getElementById("soondae-soup")
+	.addEventListener("click", () => getOrder(new Food(soondae)));
+document
+	.getElementById("hangover-soup")
+	.addEventListener("click", () => getOrder(new Food(hangover)));
 
 function getOrder(order) {
-  orderList.push(order);
-  updateOrders();
-  cook();
+	orderList.push(order);
+	updateOrders();
+	cook();
 }
 
-async function cook(){
-  const availableChef = await findAvailable(chefs);
+async function cook() {
+	const availableChef = await findAvailable(chefs);
 
-  const order = orderList.shift();
-  updateOrders();
+	const order = orderList.shift();
+	updateOrders();
 
-  const food = await availableChef.cooking(order);
-  waitingList.push(food);
-  serve();
+	const food = await availableChef.cooking(order);
+	waitingList.push(food);
+	serve();
 }
 
-async function findAvailable(staffs){
-  return new Promise((resolve) => {
-    const timerId = setInterval(()=>{
-      const availableStaff = staffs.filter((staff) => staff.status === '대기중')
-      if (availableStaff.length > 0){
-        clearInterval(timerId);
-        resolve(availableStaff[0]);
-      }
-    }, 100)
-  });
+async function findAvailable(staffs) {
+	return new Promise((resolve) => {
+		const timerId = setInterval(() => {
+			const availableStaff = staffs.find((staff) => staff.status === "대기중");
+			if (availableStaff) {
+				clearInterval(timerId);
+				resolve(availableStaff);
+			}
+		}, 100);
+	});
 }
 
-async function serve(){
-
-  const availableServer = await findAvailable(servers);
-  const food = waitingList.shift();
-  await availableServer.serving(food);
+async function serve() {
+	const availableServer = await findAvailable(servers);
+	const food = waitingList.shift();
+	await availableServer.serving(food);
 }
 
 function updateOrders() {
-  const pendingOrdersElement = document.getElementById('order');
-  pendingOrdersElement.innerHTML = orderList.map(order => `<li>주문${order.no} : ${order.name}</li>`).join('');
+	const pendingOrdersElement = document.getElementById("order");
+	pendingOrdersElement.innerHTML = orderList
+		.map((order) => `<li>주문${order.no} : ${order.name}</li>`)
+		.join("");
 }
