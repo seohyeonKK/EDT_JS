7aca167d37cb2ad5756665760aa3d83a94a0a442
seohyeonKK <sts05158@gmail.com>
2023-11-20 13:49:44 +0900

    위치에 맞게 삽입

--- StickerBox/DragAndDrop.js
+++ StickerBox/DragAndDrop.js
@@ -1,110 +1,98 @@
-import { getStickerById } from "./main.js"
+import { getStickerById } from "./main.js";
 
 export function makeStickerDraggable(element) {
-  element.onmousedown = function (event) {
-    let shiftX = event.clientX - element.getBoundingClientRect().left
-    let shiftY = event.clientY - element.getBoundingClientRect().top
+	element.onmousedown = function (event) {
+		document.body.appendChild(element);
+		let shiftX = event.clientX - element.getBoundingClientRect().left;
+		let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-    function onMouseMove(event) {
-      element.style.left = event.pageX - shiftX + "px"
-      element.style.top = event.pageY - shiftY + "px"
-    }
+		function onMouseMove(event) {
+			element.style.left = event.pageX - shiftX + "px";
+			element.style.top = event.pageY - shiftY + "px";
+		}
 
-    document.addEventListener("mousemove", onMouseMove)
-    document.addEventListener("mouseup", onMouseUp)
+		document.addEventListener("mousemove", onMouseMove);
+		document.addEventListener("mouseup", onMouseUp);
 
-    function onMouseUp() {
-      document.removeEventListener("mousemove", onMouseMove)
-      document.removeEventListener("mouseup", onMouseUp)
+		function onMouseUp() {
+			document.removeEventListener("mousemove", onMouseMove);
+			document.removeEventListener("mouseup", onMouseUp);
 
-      element.onmousemove = null
-      element.onmouseup = null
-    }
+			element.onmousemove = null;
+			element.onmouseup = null;
+		}
 
-    element.ondragstart = function () {
-      return false
-    }
-  }
+		element.ondragstart = function () {
+			return false;
+		};
+	};
 }
 
 export function makeMemoDraggable(element, parent) {
-  // newParent에 대해서 mouseEnter되면 빈 memo 그려줌
-  // newParent에 대해서 mouseLeave되면 빈 memo 삭제
-  // newParent에 대해서 Enter된 상태로
-  element.onmousedown = function (event) {
-    const left = parseInt(window.getComputedStyle(element).left)
-    const top = parseInt(window.getComputedStyle(element).top)
+	element.onmousedown = function (event) {
+		const left = parseInt(window.getComputedStyle(element).left);
+		const top = parseInt(window.getComputedStyle(element).top);
 
-    event.stopPropagation()
+		event.stopPropagation();
 
-    let shiftX = event.clientX - element.getBoundingClientRect().left
-    let shiftY = event.clientY - element.getBoundingClientRect().top
+		let shiftX = event.clientX - element.getBoundingClientRect().left;
+		let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-    /* 누르는 순간 같은 위치에 빈 memo 그려줌 */
-    const cloneMemo = element.cloneNode(false)
-    cloneMemo.style.backgroundColor = "#F65CD2"
-    parent.element.insertBefore(cloneMemo, element)
-    element.style.position = "fixed"
-    onMouseMove(event)
+		/* 누르는 순간 같은 위치에 빈 memo 그려줌 */
+		const cloneMemo = element.cloneNode(false);
+		cloneMemo.style.backgroundColor = "#F65CD2";
+		parent.element.insertBefore(cloneMemo, element);
+		element.style.position = "fixed";
+		onMouseMove(event);
 
-    function onMouseMove(event) {
-      element.style.left = event.pageX - shiftX + "px"
-      element.style.top = event.pageY - shiftY + "px"
-    }
+		function onMouseMove(event) {
+			element.style.left = event.pageX - shiftX + "px";
+			element.style.top = event.pageY - shiftY + "px";
+		}
 
-    /* parent에 대해서 mouseLeave되면 빈 memo 삭제 */
-    parent.element.addEventListener("mouseleave", removeCloneMemo)
-    function removeCloneMemo() {
-      if (cloneMemo !== undefined) cloneMemo.remove()
-      parent.element.removeEventListener("mouseleave", removeCloneMemo)
-    }
+		/* parent에 대해서 mouseLeave되면 빈 memo 삭제 */
+		parent.element.addEventListener("mouseleave", removeCloneMemo);
+		function removeCloneMemo() {
+			if (cloneMemo !== undefined) cloneMemo.remove();
+			parent.element.removeEventListener("mouseleave", removeCloneMemo);
+		}
 
-    // box에 한정되는 것이 아니라 박스를 잡은 순간에는
-    // 전체 document에 mousemove 핸들링을 걸어줌
-    document.addEventListener("mousemove", onMouseMove)
+		// box에 한정되는 것이 아니라 박스를 잡은 순간에는
+		// 전체 document에 mousemove 핸들링을 걸어줌
+		document.addEventListener("mousemove", onMouseMove);
 
-    // 마우스를 놓았을 때 박스를 내려놓아야 하므로
-    // 전체 document에 걸려있던 mousemove 핸들링 삭제함
-    document.addEventListener("mouseup", onMouseUp)
+		// 마우스를 놓았을 때 박스를 내려놓아야 하므로
+		// 전체 document에 걸려있던 mousemove 핸들링 삭제함
+		document.addEventListener("mouseup", onMouseUp);
 
-    function onMouseUp() {
-      const currentRect = element.getBoundingClientRect()
-      removeCloneMemo()
-      document.removeEventListener("mousemove", onMouseMove)
-      document.removeEventListener("mouseup", onMouseUp)
+		function onMouseUp() {
+			const currentRect = element.getBoundingClientRect();
+			removeCloneMemo();
+			document.removeEventListener("mousemove", onMouseMove);
+			document.removeEventListener("mouseup", onMouseUp);
 
-      const newParentSticker = getNewParentSticker(
-        currentRect.left,
-        currentRect.top
-      )
-      if (
-        newParentSticker !== undefined &&
-        parseInt(newParentSticker.id) !== parent.id
-      ) {
-        parent.removeMemo(element)
-        // TODO: 바꿔야함
-        getStickerById(parseInt(newParentSticker.id)).addMemoByButton(element)
-      } else {
-        element.style.position = "relative"
-        element.style.left = left + "px"
-        element.style.top = top + "px"
-      }
-    }
+			const newParentSticker = getNewParentSticker(currentRect.left, currentRect.top);
+			if (newParentSticker !== undefined && parseInt(newParentSticker.id) !== parent.id) {
+				parent.removeMemo(element);
+				const index = parseInt((currentRect.top - 72) / 55);
+				getStickerById(parseInt(newParentSticker.id)).addMemoByIndex(element, index);
+			} else {
+				element.style.position = "relative";
+				element.style.left = left + "px";
+				element.style.top = top + "px";
+			}
+		}
 
-    element.onmousemove = null
-    element.onmouseup = null
-  }
+		element.onmousemove = null;
+		element.onmouseup = null;
+	};
 
-  element.ondragstart = function () {
-    return false
-  }
+	element.ondragstart = function () {
+		return false;
+	};
 }
 
 function getNewParentSticker(x, y) {
-  const elements = document.elementsFromPoint(x, y)
-  return elements.find((element) => element.className === "sticker")
+	const elements = document.elementsFromPoint(x, y);
+	return elements.find((element) => element.className === "sticker");
 }
-
-// newParent를 계속해서 찾고 newParent 내에서의 좌표를 구해 위치를 정한다.. ?
-//
-function showDropPosition(event) {}
--- StickerBox/Sticker.js
+++ StickerBox/Sticker.js
@@ -1,100 +1,126 @@
-import { makeStickerDraggable, makeMemoDraggable } from "./DragAndDrop.js"
+import { makeStickerDraggable, makeMemoDraggable } from "./DragAndDrop.js";
 
-let no = 1
+let no = 1;
 
 export class Sticker {
-  id = 0
-  element = null
-  memoList = []
-  memoId = 0
+	id = 0;
+	element = null;
+	memoList = [];
+	memoId = 0;
 
-  constructor() {
-    this.id = no++
-    this.element = null
-    this.memoList = []
-    this.memoId = 0
-    this.createStickerElement()
-  }
+	constructor() {
+		this.id = no++;
+		this.element = null;
+		this.memoList = [];
+		this.memoId = 0;
+		this.createStickerElement();
+	}
 
-  getElement() {
-    return this.element
-  }
+	getElement() {
+		return this.element;
+	}
 
-  getMemoList() {
-    return this.memoList
-  }
+	getMemoList() {
+		return this.memoList;
+	}
 
-  setMemoList(memoList) {
-    this.memoList = memoList
-  }
+	setMemoList(memoList) {
+		this.memoList = memoList;
+	}
 
-  createStickerElement() {
-    // sticker 생성
-    const stickerEl = document.createElement("div")
-    stickerEl.id = this.id
-    stickerEl.className = "sticker"
-    stickerEl.style.left = 20 + 7 * this.id + "px"
-    stickerEl.style.top = 20 + 7 * this.id + "px"
-    stickerEl.style.backgroundColor = `rgb(
+	createStickerElement() {
+		// sticker 생성
+		const stickerEl = document.createElement("div");
+		stickerEl.id = this.id;
+		stickerEl.className = "sticker";
+		stickerEl.style.left = 20 + 7 * this.id + "px";
+		stickerEl.style.top = 20 + 7 * this.id + "px";
+		stickerEl.style.backgroundColor = `rgb(
 			${150 + Math.random() * 50},
 			${150 + Math.random() * 50},
-			${150 + Math.random() * 50}, 0.9)`
+			${150 + Math.random() * 50}, 0.9)`;
 
-    // 항목추가 버튼
-    const addMemoButton = document.createElement("button")
-    addMemoButton.textContent = "항목 추가"
-    addMemoButton.onclick = () => this.addMemoByButton()
-    stickerEl.appendChild(addMemoButton)
+		// 항목추가 버튼
+		const addMemoButton = document.createElement("button");
+		addMemoButton.textContent = "항목 추가";
+		addMemoButton.onmouseup = () => this.addMemoByButton();
+		stickerEl.appendChild(addMemoButton);
 
-    // 스티커 삭제 버튼
-    const removeMemoButton = document.createElement("button")
-    removeMemoButton.textContent = "스티커 삭제"
-    removeMemoButton.onclick = () => this.removeSticker()
-    stickerEl.appendChild(removeMemoButton)
+		// 스티커 삭제 버튼
+		const removeMemoButton = document.createElement("button");
+		removeMemoButton.textContent = "스티커 삭제";
+		removeMemoButton.onmouseup = () => this.removeSticker();
+		stickerEl.appendChild(removeMemoButton);
 
-    // 드래그 가능하도록 이벤트 등록
-    makeStickerDraggable(stickerEl)
+		// 드래그 가능하도록 이벤트 등록
+		makeStickerDraggable(stickerEl);
 
-    this.element = stickerEl
-    document.body.appendChild(stickerEl)
-  }
+		this.element = stickerEl;
+		document.body.appendChild(stickerEl);
+	}
 
-  // 항목 추가
-  addMemoByButton() {
-    const memoEl = document.createElement("div")
-    memoEl.className = "memo"
-    memoEl.id = "memo" + ++this.memoId
-    memoEl.style.top = 100 + 35 * this.memoId
-    // 텍스트
-    const memoTextNode = document.createTextNode("sampel Text" + this.memoId)
-    memoEl.appendChild(memoTextNode)
+	// 항목 추가
+	addMemoByButton() {
+		const memoEl = document.createElement("div");
+		memoEl.className = "memo";
+		memoEl.id = "memo" + ++this.memoId;
+		memoEl.style.top = 100 + 35 * this.memoId;
+		// 텍스트
+		const memoText = document.createElement("span");
+		memoText.innerText = "sampel Text" + this.memoId;
+		memoEl.appendChild(memoText);
 
-    // 삭제 버튼
-    const memoRemoveButton = document.createElement("button")
-    memoRemoveButton.textContent = "삭제"
-    memoRemoveButton.onclick = (event) => this.removeMemoByButton(event)
-    memoEl.appendChild(memoRemoveButton)
+		// 삭제 버튼
+		const memoRemoveButton = document.createElement("button");
+		memoRemoveButton.textContent = "삭제";
+		memoRemoveButton.onmouseup = (event) => this.removeMemoByButton(event);
+		memoEl.appendChild(memoRemoveButton);
 
-    makeMemoDraggable(memoEl, this)
+		makeMemoDraggable(memoEl, this);
+		this.memoList.push(memoEl);
+		this.renderMemos();
+	}
 
-    this.element.appendChild(memoEl)
-    this.memoList.push(memoEl)
-  }
+	addMemoByIndex(element, index) {
+		const memoEl = document.createElement("div");
+		memoEl.className = "memo";
+		memoEl.id = "memo" + ++this.memoId;
 
-  removeMemoByButton(event) {
-    this.memoList = this.memoList.filter(
-      (memo) => memo.id !== event.target.parentElement.id
-    )
-    event.target.parentElement.remove()
-  }
+		// 텍스트
+		const memoText = document.createElement("span");
+		memoText.innerText = element.getElementsByTagName("span")[0].innerText;
+		memoEl.appendChild(memoText);
 
-  removeMemo(targetMemo) {
-    this.memoList = this.memoList.filter((memo) => memo.id !== targetMemo.id)
-    targetMemo.remove()
-  }
+		// 삭제 버튼
+		const memoRemoveButton = document.createElement("button");
+		memoRemoveButton.textContent = "삭제";
+		memoRemoveButton.onmouseup = (event) => this.removeMemoByButton(event);
+		memoEl.appendChild(memoRemoveButton);
 
-  // 스티커 삭제
-  removeSticker() {
-    this.element.remove()
-  }
+		makeMemoDraggable(memoEl, this);
+
+		this.memoList.splice(index, 0, memoEl);
+		this.renderMemos();
+	}
+
+	removeMemoByButton(event) {
+		this.memoList = this.memoList.filter(
+			(memo) => memo.id !== event.target.parentElement.id
+		);
+		event.target.parentElement.remove();
+	}
+
+	removeMemo(targetMemo) {
+		this.memoList = this.memoList.filter((memo) => memo.id !== targetMemo.id);
+		targetMemo.remove();
+	}
+
+	// 스티커 삭제
+	removeSticker() {
+		this.element.remove();
+	}
+
+	renderMemos() {
+		this.memoList.forEach((memo) => this.element.appendChild(memo));
+	}
 }
